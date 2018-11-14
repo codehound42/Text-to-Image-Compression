6b63102545ee9858813b6ddee974eaa37839ba97
codehound42 <codehound42@gmail.com>
2018-11-14 21:48:44 +0100

    Refactor image rendering
    TODO: Consider using RGB image instead of greyscale image to allow for bigger value range.

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ def render_image(phrases):
     height = int(np.sqrt(len(phrases)))
     width = 2*height
     
-    new_im = Image.new("L", (width,height))
     
     current_phrase_index = 0
-    pixels = np.zeros((height, width), dtype=np.uint8)
+    pixels = np.zeros(height*width, dtype=np.uint8)
     for i in range(height):
         for j in range(0,width,2):
             if current_phrase_index >= len(phrases):
@@ -19,23 +18,15 @@ def render_image(phrases):
             phrase_pointer = phrases[current_phrase_index][0]
             char_val = ord(phrases[current_phrase_index][1])
             
-            pixels[i, j] = phrase_pointer
-            pixels[i, j+1] = char_val
-            
-            new_im.putpixel((j,i), phrase_pointer)
-            new_im.putpixel((j+1,i), char_val)
+            pixels[i*width + j] = phrase_pointer
+            pixels[i*width + j+1] = char_val
             
             current_phrase_index += 1
     
-    pixels[0,0] = 10000
-    print(pixels)
-    
-    #new_im.putdata(pixels)
-    #new_im.putpixel(pixels)
+    # Render image
+    new_im = Image.new("L", (width,height))
+    new_im.putdata(pixels)
     new_im.save("test.png")
-    
-    #img = Image.fromarray(pixels, 'RBG')
-    #img.save('test.png')
     #img.show()
     
     print('done rendering')
