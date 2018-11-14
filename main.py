a6cb34759cb200b8d077b290b4afc6426b2e8794
codehound42 <codehound42@gmail.com>
2018-11-14 22:14:50 +0100

    Use RGB images instead of greyscale images

--- main.py
+++ main.py
@@ -1,44 +1,40 @@
 import compression as cp
 import numpy as np
-#import scipy.misc as smp
 from PIL import Image
+
+def int_to_rgb_values(val):
+    red = (val >> 16) & 255
+    green = (val >> 8) & 255
+    blue = val & 255
+    return red, green, blue
 
 def render_image(phrases):
     height = int(np.sqrt(len(phrases)))
     width = 2*height
     
-    
     current_phrase_index = 0
-    pixels = np.zeros(height*width, dtype=np.uint8)
+    pixels = list()
     for i in range(height):
-        for j in range(0,width,2):
-            if current_phrase_index >= len(phrases):
-                break
-            
+        for j in range(0,width,2):            
             phrase_pointer = phrases[current_phrase_index][0]
             char_val = ord(phrases[current_phrase_index][1])
             
-            pixels[i*width + j] = phrase_pointer
-            pixels[i*width + j+1] = char_val
+            red1, green1, blue1 = int_to_rgb_values(phrase_pointer)
+            red2, green2, blue2 = int_to_rgb_values(char_val)
+            
+            pixels.append((red1, green1, blue1))
+            pixels.append((red2, green2, blue2))
             
             current_phrase_index += 1
     
+    print(pixels)
+    
     # Render image
-    new_im = Image.new("L", (width,height))
-    new_im.putdata(pixels)
-    new_im.save("test.png")
-    #img.show()
+    img = Image.new("RGB", (width,height))
+    img.putdata(pixels)
+    img.save("test.png")
     
     print('done rendering')
-    
-
-def test():
-    w, h = 512, 512
-    data = np.zeros((h, w, 3), dtype=np.uint8)
-    data[256, 256] = [255, 0, 0]
-    img = Image.fromarray(data, 'RGB')
-    img.save('test.png')
-    #img.show()
     
 def main():
     file_name = 'sample_text_file.txt'
