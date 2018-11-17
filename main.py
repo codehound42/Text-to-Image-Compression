9725ffb6560d35c34fc760dffc28ea6bb64b1850
codehound42 <codehound42@gmail.com>
2018-11-17 10:40:08 +0100

    Implement image reading + output file generation
    Read pixels in image -> Convert to phrases -> Decode -> Write message to file

--- main.py
+++ main.py
@@ -8,7 +8,33 @@ def int_to_rgb_values(val):
     blue = val & 255
     return red, green, blue
 
-def render_image(phrases):
+def rgb_values_to_int(red, green, blue):
+    return blue + (green * 16**2) + (red * 16**4)
+    
+def read_image(file_path):
+    img = Image.open(file_path)
+    img_rgb = img.convert('RGB')
+    
+    width, height = img.size
+    phrases = []
+    
+    for i in range(height):
+        for j in range(0,width,2):
+            red1, green1, blue1 = img_rgb.getpixel((j, i))
+            red2, green2, blue2 = img_rgb.getpixel((j+1, i))
+            
+            phrase_pointer = rgb_values_to_int(red1, green1, blue1)
+            char = chr(rgb_values_to_int(red2, green2, blue2))
+            phrases.append((phrase_pointer, char))
+    
+    print(phrases)
+    return phrases
+
+def generate_original_file(file_path, file_contents):
+    file = open(file_path, 'w') # Create a file if it does not already exist
+    file.write(file_contents)
+
+def render_image(file_path, phrases):
     height = int(np.sqrt(len(phrases)))
     width = 2*height
     
@@ -32,22 +58,27 @@ def render_image(phrases):
     # Render image
     img = Image.new("RGB", (width,height))
     img.putdata(pixels)
-    img.save("test.png")
+    img.save(file_path)
     
-    print('done rendering')
     
+# Quick simple manual test
 def main():
     file_name = 'sample_text_file.txt'
     file_compresser = cp.FileCompresser()
     
     file_compresser.read_file(file_name)
     phrases = file_compresser.encode()
-    original_string = file_compresser.decode(phrases)
     print(phrases)
-    print(original_string)
     
-    render_image(phrases)
-    #test()
+    file_path = 'message_image.png'
+    render_image(file_path, phrases)
+    print('done rendering')
+    phrases = read_image(file_path)
+    print('done reading image')
+    original_string = file_compresser.decode(phrases)
+    print(original_string)
+    generate_original_file('out.txt', original_string)
+    print('done writing compressed message contents to file')
 
 if __name__ == '__main__':
     main()
